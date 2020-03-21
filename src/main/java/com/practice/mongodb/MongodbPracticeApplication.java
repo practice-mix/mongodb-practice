f2a4c119a76ac7a0a0fa2aaf6c31af071f3fe8c1
LeonGeroge <leongeroge@gmail.com>
2020-03-22 00:21:25 +0800

    init

---
+++ src/main/java/com/practice/mongodb/MongodbPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.practice.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbPracticeApplication.class, args);
+    }
+}
