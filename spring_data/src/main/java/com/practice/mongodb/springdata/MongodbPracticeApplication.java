cb4816ebbd555c0855e37222ccd6e9f09b0849d2
leongeroge <leongeroge@outlook.com>
2022-03-07 17:19:34 +0800

    use java 11

--- spring_data/src/main/java/com/practice/mongodb/springdata/MongodbPracticeApplication.java
+++ spring_data/src/main/java/com/practice/mongodb/springdata/MongodbPracticeApplication.java
@@ -1,10 +1,15 @@
 package com.practice.mongodb.springdata;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class MongodbPracticeApplication {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(MongodbPracticeApplication.class, args);
