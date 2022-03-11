60093e78cde6d3761b696ac485e411fe2fa3f2f9
leongeroge <leongeroge@outlook.com>
2022-03-11 18:02:23 +0800

    [java-driver] aggreate: group

--- native/src/main/java/com/practice/mongodb/primitive/odm/ZipsService.java
+++ native/src/main/java/com/practice/mongodb/primitive/odm/ZipsService.java
@@ -6,6 +6,9 @@ import com.mongodb.client.*;
 import com.mongodb.client.model.Accumulators;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
+import com.practice.mongodb.primitive.odm.entity.Zips;
+import lombok.Data;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
@@ -13,12 +16,12 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import static java.util.Arrays.asList;
 
 public class ZipsService {
     private static MongoClient mongoClient;
     private static MongoDatabase database;
-    private static MongoCollection<Document> collection;
+    private static MongoCollection<Zips> collection;
 
     /**
      * @see ConnectionString
@@ -42,41 +45,149 @@ public class ZipsService {
 //        mongoClient = MongoClients.create("mongodb://localhost:27017/?maxPoolSize=100&waitQueueTimeoutMS=3000&maxConnecting=5");
 
         database = mongoClient.getDatabase("test");
-        collection = database.getCollection("zips");
+        collection = database.getCollection("zips", Zips.class);
 
     }
 
-    /**
-     * <pre> {@code
-     * db.zips.aggregate([
-     * {
-     * $group: {
-     * _id: "$state",
-     * totalPop: {
-     * $sum: "$pop"
-     * }
-     * }
-     * }, {
-     * $match: {
-     * totalPop: {
-     * $gte: 10 * 1000 * 1000
-     * }
-     * }
-     * }
-     *
-     * ])
-     * }</pre>
+    /*
+db.zips.aggregate([
+    {
+        $group: {
+            _id: "$state",
+            totalPop: {
+                $sum: "$pop"
+            }
+        }
+    }, {
+        $match: {
+            totalPop: {
+                $gte: 10 * 1000 * 1000
+            }
+        }
+    }
+
+])
      */
     @Test
     public void aggregate1() {
-        AggregateIterable<Document> aggregate = collection.aggregate(Arrays.asList(
+        AggregateIterable<Root1> aggregate = collection.aggregate(asList(
                 Aggregates.group("$state", Accumulators.sum("totalPop", "$pop")),
                 Aggregates.match(Filters.gte("totalPop", 10 * 1000 * 1000))
-        ));
+        ), Root1.class);
 
-        for (Document document : aggregate) {
-            System.out.println(document.toJson());
+        for (Root1 document : aggregate) {
+            System.out.println(document);
         }
 
     }
+
+    @Data
+    public static class Root1 {
+        public String _id;
+        public int totalPop;
+    }
+
+    /*
+     <pre>
+     {@code
+db.zips.aggregate([
+    {
+        $group: {
+            _id: {
+                state: "$state",
+                city: "$city"
+            },
+            cityPop: {
+                $sum: "$pop"
+            }
+        }
+    },
+    {
+        $group: {
+            _id: "$_id.state",
+            avgPop: {
+                $avg: "$cityPop"
+            }
+        }
+    }
+])
+     }
+     </pre>
+
+     */
+    @Test
+    public void aggregate2() {
+        collection.aggregate(asList(
+                Aggregates.group(Document.parse("{\n" +
+                        "                state: \"$state\",\n" +
+                        "                city: \"$city\"\n" +
+                        "            }"), Accumulators.sum("cityPop", "$pop")),
+                Aggregates.group("$_id.state", Accumulators.avg("avgPop", "$cityPop"))
+
+        ), Root2.class).forEach(root -> System.out.println(root));
+    }
+
+    @Data
+    public static class Root2 {
+        public String _id;
+        public double avgPop;
+    }
+
+
+    @Test
+    public void aggregate3() {
+        collection.aggregate(asList(
+                        Aggregates.group(Document.parse("{\n" +
+                                "                state: \"$state\",\n" +
+                                "                city: \"$city\"\n" +
+                                "            }"), Accumulators.sum("cityPop", "$pop"))
+                        , Aggregates.sort(Sorts.ascending("cityPop"))
+                        , Aggregates.group("$_id.state", asList(
+                                        Accumulators.last("maxCity", "$_id.city"),
+                                        Accumulators.last("maxPop", "$cityPop"),
+                                        Accumulators.first("minCity", "$_id.city"),
+                                        Accumulators.first("minPop", "$cityPop")
+
+                                )
+
+                        ),
+                        Aggregates.project(Document.parse("{\n" +
+                                "            _id: 0,\n" +
+                                "            state: \"$_id\",\n" +
+                                "            max: {\n" +
+                                "                city: \"$maxCity\",\n" +
+                                "                pop: \"$maxPop\"\n" +
+                                "            },\n" +
+                                "            min: {\n" +
+                                "                city: \"$minCity\",\n" +
+                                "                pop: \"$minPop\"\n" +
+                                "            }\n" +
+                                "        }"))
+
+                )
+                , Root3.class
+        ).forEach(doc -> System.out.println(doc.toString()));
+
+    }
+
+    @Data
+    public static class Max {
+        public String city;
+        public int pop;
+    }
+
+    @Data
+    public static class Min {
+        public String city;
+        public int pop;
+    }
+
+    @Data
+    public static class Root3 {
+        public String state;
+        public Max max;
+        public Min min;
+    }
+
+
 }
